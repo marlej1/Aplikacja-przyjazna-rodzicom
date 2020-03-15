ab9d3bd9bd9e2213c21a6910d0838be8ced7e948
Marcin Lejko <marcin.lejko@gmail.com>
2020-03-15 22:22:07 +0100

    Login with JWT

--- BoboTu/BoboTu.Web/Controllers/AccountController.cs
+++ BoboTu/BoboTu.Web/Controllers/AccountController.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using BoboTu.Data;
 using BoboTu.Data.Models;
 using BoboTu.Data.Repositories;
 using BoboTu.Web.Dtos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BoboTu.Web.Controllers
 {
@@ -17,10 +22,14 @@ namespace BoboTu.Web.Controllers
     public class AccountController : ControllerBase
     {
         private readonly IAuthRepository _authRepository;
+        private readonly IConfiguration _config;
 
-        public AccountController(IAuthRepository authRepository)
+        
+
+        public AccountController(IAuthRepository authRepository, IConfiguration config)
         {
             _authRepository = authRepository;
+            _config = config;
         }
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userDto)
@@ -40,6 +49,49 @@ namespace BoboTu.Web.Controllers
             return StatusCode(201);
 
 
+        }
+        [HttpPost("login")]
+
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _authRepository.Login(userForLoginDto.UserName, userForLoginDto.Password);
+
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new []
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
+
+        }
+
+        [HttpGet("test")]
+        [Authorize]
+
+        public async Task<IActionResult> Test()
+        {
+            return Ok("Masz uprawnienia");
         }
     }
 }
--- BoboTu/BoboTu.Web/Startup.cs
+++ BoboTu/BoboTu.Web/Startup.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using BoboTu.Data;
 using BoboTu.Data.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +15,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BoboTu.Web
 {
@@ -32,6 +35,19 @@ namespace BoboTu.Web
             services.AddDbContext<BoboTuDbContext>(o => o.UseSqlServer(Configuration.GetConnectionString("BoboTuConnection")));
             services.AddScoped<IAuthRepository, AuthRepository>();
             services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+               .AddJwtBearer(options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                       ValidateIssuerSigningKey = true,
+                       IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII
+                           .GetBytes(Configuration.GetSection("AppSettings:Token").Value)),
+                       ValidateIssuer = false,
+                       ValidateAudience = false
+                   };
+               });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +61,8 @@ namespace BoboTu.Web
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
